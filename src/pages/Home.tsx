34cb346e0922bb6b54cf2f5756605ad27062a83c
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-22 18:29:41 +0330

    refactor(Home): project named to Breeze

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,77 +7,79 @@ import { useAuth } from '../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
-    const [currentWeather, setCurrentWeather] = useState<CurrentWeather | null>(null);
-    const [forecastData, setForecastData] = useState<ForecastData[] | null>(null);
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(false); // Add loading state
+  const [currentWeather, setCurrentWeather] = useState<CurrentWeather | null>(null);
+  const [forecastData, setForecastData] = useState<ForecastData[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false); // Add loading state
 
-    const { handleLogout } = useAuth();
-    const navigate = useNavigate();
+  const { handleLogout } = useAuth();
+  const navigate = useNavigate();
 
-    const handleSearch = async (city: string) => {
-        try {
-            setLoading(true); // Start loading
-            setError(null); // Reset error state
-            const { lat, lon } = await getCoordinates(city);
-            const current = await getCurrentWeather(lat, lon);
-            const forecast = await getForecast(lat, lon);
-            setCurrentWeather(current);
-            setForecastData(forecast);
-        } catch (err) {
-            setError('Failed to fetch weather data. Please try again.');
-        } finally {
-            setLoading(false); // Stop loading
-        }
-    };
+  const handleSearch = async (city: string) => {
+    try {
+      setLoading(true); // Start loading
+      setError(null); // Reset error state
+      const { lat, lon } = await getCoordinates(city);
+      const current = await getCurrentWeather(lat, lon);
+      const forecast = await getForecast(lat, lon);
+      setCurrentWeather(current);
+      setForecastData(forecast);
+    } catch (err) {
+      setError('Failed to fetch weather data. Please try again.');
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  };
 
-    const handleLogoutClick = () => {
-        handleLogout();
-        navigate('/login');
-    };
+  const handleLogoutClick = () => {
+    handleLogout();
+    navigate('/login');
+  };
 
-    return (
-        <Box
-            sx={{
-                minHeight: '100vh',
-                backgroundImage: 'url(src/assets/images/weather-background.jpg)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundAttachment: 'fixed',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Container>
-                <Typography variant="h3" align="center" gutterBottom sx={{ mt: 3 }}>
-                    Weather App
-                </Typography>
-                <SearchBar onSearch={handleSearch} />
-                {loading ? (
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '200px' }}>
-                        <CircularProgress size={60} thickness={4} color="primary"  />
-                    </Box>
-                ) : error ? (
-                    <Typography variant="h6" color="error" align="center">
-                        {error}
-                    </Typography>
-                ) : (
-                    <WeatherDisplay currentWeather={currentWeather} forecastData={forecastData} />
-                )}
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    fullWidth
-                    onClick={handleLogoutClick}
-                    sx={{ mt: 3, mb: 3 }}
-                >
-                    Logout
-                </Button>
-                <Typography variant="h6" color="white">Please use VPN.</Typography>
-            </Container>
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        backgroundImage: 'url(src/assets/images/weather-background.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Container>
+        <Typography variant="h3" align="center" gutterBottom sx={{ mt: 3,}}>
+          Breeze
+        </Typography>
+        <SearchBar onSearch={handleSearch} />
+        {loading ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '200px' }}>
+            <CircularProgress size={60} thickness={4} color="primary" />
+          </Box>
+        ) : error ? (
+          <Typography variant="h6" color="error" align="center">
+            {error}
+          </Typography>
+        ) : (
+          <WeatherDisplay currentWeather={currentWeather} forecastData={forecastData} />
+        )}
+        <Box sx={{}}>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={handleLogoutClick}
+            sx={{ mt: 3, mb: 3 }}
+          >
+            Logout
+          </Button>
+          <Typography variant="h6" color="white">Please use VPN.</Typography>
         </Box>
-    );
+      </Container>
+    </Box>
+  );
 };
 
 export default Home;
